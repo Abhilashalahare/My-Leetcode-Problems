cad11b9b9f4c331d1f9118243d980bad27af0555
Abhilashalahare <166977285+Abhilashalahare@users.noreply.github.com>
2024-08-14 16:25:00 +0530

    Time: 0 ms (100%), Space: 42.2 MB (49.22%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,33 @@
+class Solution {
+    public double myPow(double x, int n) {
+        return find(x,(long) n);
+    }
+
+    double find(double x, long n){
+
+    
+        if(n==0){
+            return 1;
+        }
+        if(x==0){
+            return 0;
+        }
+
+    if(n<0){
+        return 1.0/find(x,-n);
+    }
+
+        double half = find(x,n/2);
+        double sq = half*half;
+
+
+        if(n%2!=0){
+            sq = x*sq;
+        }
+
+
+        return sq;
+    
+       }
+        
+    }
