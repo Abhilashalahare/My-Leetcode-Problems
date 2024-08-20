8b2b23f1ad91960d984e706d98e13119f9a366bf
Abhilashalahare <166977285+Abhilashalahare@users.noreply.github.com>
2024-08-20 08:33:50 +0530

    Time: 0 ms (100%), Space: 40.7 MB (41.51%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -6,9 +6,7 @@ class Solution {
         if(n==1){
             return true;
         }
-        if(n%2==1){
-            return false;
-        }
+      
 
         if(n%4 != 0){
             return false;
