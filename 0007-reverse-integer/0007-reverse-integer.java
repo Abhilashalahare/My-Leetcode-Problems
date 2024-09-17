200b5d47b888196efa40825e2df5749e953e088f
Abhilashalahare <166977285+Abhilashalahare@users.noreply.github.com>
2024-09-17 12:43:28 +0530

    Time: 1 ms (83.81%), Space: 40.3 MB (97.52%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int reverse(int x) {
+        long sum=0;
+        while(x !=0){
+            sum = sum*10 + x%10;
+            x=x/10;
+            
+        }
+        return (sum<Integer.MIN_VALUE || sum>Integer.MAX_VALUE) ? 0 : (int)sum;
+    }
+}
