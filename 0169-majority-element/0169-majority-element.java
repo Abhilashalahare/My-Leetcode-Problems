ce94c67f2cf70d1886ba2a68158488fb044d2035
Abhilashalahare <166977285+Abhilashalahare@users.noreply.github.com>
2024-09-26 17:43:43 +0530

    Time: 1 ms (99.79%), Space: 52.9 MB (51.52%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,8 +1,33 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        //when we will sort this array the middle element will always lie in the majority elements.
-        int mid = (nums.length)/2;
-        Arrays.sort(nums);
-        return nums[mid];
+        int elet =0;
+        int cnt=0;
+
+        for(int i=0; i<nums.length; i++){
+            if(cnt == 0){
+                cnt =1;
+                elet = nums[i];
+            }else if(elet == nums[i]){
+                cnt++;
+            }else{
+                cnt--;
+            }
+
+        }
+
+            int cnt1=0;
+            for(int i=0; i<nums.length; i++){
+                if(nums[i] == elet){
+                    cnt1++;
+                }
+            
+           
+            }
+        
+
+         if(cnt1 > (nums.length)/2){
+                return elet;
+            }
+            return -1;
     }
 }
