39227806dd6fc4315428f241f86e7a4d0470ec31
Abhilashalahare <166977285+Abhilashalahare@users.noreply.github.com>
2024-09-27 00:17:37 +0530

    Time: 3 ms (73.96%), Space: 47.9 MB (49.85%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,22 +1,42 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        ArrayList<Integer> arr = new ArrayList<>();
-        for(int i=0; i<nums.length; i++){
-           
-            if(map.containsKey(nums[i])){
-                map.put(nums[i],map.get(nums[i])+1);
-            }else{
-                map.put(nums[i],1);
-            }
+      int cnt1=0, cnt2=0;
+      int e1=Integer.MIN_VALUE,e2=Integer.MIN_VALUE;
 
-            if(map.get(nums[i])==(int)(nums.length/3)+1){
-                arr.add(nums[i]);
-            }
-            
+      for(int i=0; i<nums.length; i++){
+        if(cnt1==0 && nums[i]!=e2){
+            cnt1 = 1;
+            e1 = nums[i];
+        }else if(cnt2==0 && nums[i]!=e1){
+            cnt2=1;
+            e2=nums[i];
         }
-        
+        else if(e1==nums[i]) cnt1++;
+        else if(e2==nums[i]) cnt2++;
 
-        return arr;
+        else{
+            cnt1--;
+            cnt2--;
+        }
+      }
+          List<Integer> ls = new ArrayList<>(); // list of answers
+
+        // Manually check if the stored elements in
+        // el1 and el2 are the majority elements:
+        cnt1 = 0; cnt2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == e1) cnt1++;
+            if (nums[i] == e2) cnt2++;
+        }
+
+        int mini = (int)(nums.length / 3) + 1;
+        if (cnt1 >= mini) ls.add(e1);
+        if (cnt2 >= mini) ls.add(e2);
+
+        // Uncomment the following line
+        // if it is told to sort the answer array:
+        //Collections.sort(ls); //TC --> O(2*log2) ~ O(1);
+
+        return ls;
     }
 }
