56f18c14e7512d3d32e3ff8525d58671c0148d0d
Abhilashalahare <166977285+Abhilashalahare@users.noreply.github.com>
2024-11-12 22:22:29 +0530

    Time: 21 ms (50.84%), Space: 45.4 MB (22.93%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,40 +1,45 @@
 class Solution {
-      public static String longestPalindrome(String s) {
-        if (s == null || s.length() < 2) {
-            return s;
-        }
-        
-        int start = 0, end = 0;
-        
-        for (int i = 0; i < s.length(); i++) {
-            // Check for odd-length palindromes with center i
-            int len1 = expandFromCenter(s, i, i);
-            
-            // Check for even-length palindromes with center between i and i+1
-            int len2 = expandFromCenter(s, i, i + 1);
-            
-            // Choose the longer palindrome length
-            int len = Math.max(len1, len2);
-            
-            // Update start and end indices if we found a longer palindrome
-            if (len > end - start) {
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
-            }
-        }
-        
-        // Extract and return the longest palindrome substring
-        return s.substring(start, end + 1);
+    public String longestPalindrome(String str) {
+        if (str.length() <= 1)
+      return str;
+
+    String LPS = "";
+
+    for (int i = 1; i < str.length(); i++) {
+
+      // Consider odd length
+      int low = i;
+      int high = i;
+      while(str.charAt(low) == str.charAt(high)) {
+        low--;
+        high++;
+
+        if (low == -1 || high == str.length())
+          break;
+      }
+
+      String palindrome = str.substring(low+1, high);
+      if (palindrome.length() > LPS.length()) {
+        LPS = palindrome;
+      }
+
+      // Consider even length
+      low = i-1;
+      high = i;
+      while(str.charAt(low) == str.charAt(high)) {
+        low--;
+        high++;
+
+        if (low == -1 || high == str.length())
+          break;
+      }
+
+      palindrome = str.substring(low+1, high);
+      if (palindrome.length() > LPS.length()) {
+        LPS = palindrome;
+      }
     }
-    
-    // Helper method to expand from center and find palindrome length
-    private static int expandFromCenter(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
-        }
-        
-        // Length of palindrome is right - left - 1
-        return right - left - 1;
+
+    return LPS;
+  }
     }
-}
