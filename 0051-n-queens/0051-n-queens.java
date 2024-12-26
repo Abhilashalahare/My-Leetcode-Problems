972c66a6062bc80ec4c16b2295e962c6c5062133
Abhilashalahare <166977285+Abhilashalahare@users.noreply.github.com>
2024-12-26 12:43:02 +0530

    Time: 2 ms (88.02%), Space: 45.2 MB (10.7%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,63 +1,63 @@
 class Solution {
+   static List<List<String>> allSolutions = new ArrayList<>();
+
     public List<List<String>> solveNQueens(int n) {
           char[][] board = new char[n][n];
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                board[i][j] = '.';
-        List < List < String >> res = new ArrayList < List < String >> ();
-        dfs(0, board, res);
-        return res;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.'; // Empty board
+            }
+        }
+        allSolutions.clear(); // Clear previous results if any
+        nQueens(board, 0);
+        return allSolutions;
     }
- static boolean validate(char[][] board, int row, int col) {
-        int duprow = row;
-        int dupcol = col;
-        while (row >= 0 && col >= 0) {
-            if (board[row][col] == 'Q') return false;
-            row--;
-            col--;
-        }
-
-        row = duprow;
-        col = dupcol;
-        while (col >= 0) {
-            if (board[row][col] == 'Q') return false;
-            col--;
-        }
-
-        row = duprow;
-        col = dupcol;
-        while (col >= 0 && row < board.length) {
-            if (board[row][col] == 'Q') return false;
-            col--;
-            row++;
-        }
-        return true;
-    }
-
-    static void dfs(int col, char[][] board, List < List < String >> res) {
-        if (col == board.length) {
-            res.add(construct(board));
+    public static void nQueens(char[][] board, int row) {
+        // Base case: all queens are placed
+        if (row == board.length) {
+            // Convert the board to a list of strings and add to solutions
+            List<String> solution = new ArrayList<>();
+            for (int i = 0; i < board.length; i++) {
+                solution.add(new String(board[i]));
+            }
+            allSolutions.add(solution);
             return;
         }
 
-        for (int row = 0; row < board.length; row++) {
-            if (validate(board, row, col)) {
+        // Try placing queen in each column
+        for (int col = 0; col < board.length; col++) { //same row pe alg alg col pe dal k dekhna h Q ko agr safe h to
+            if (isSafe(board, row, col)) {
                 board[row][col] = 'Q';
-                dfs(col + 1, board, res);
+                nQueens(board, row + 1);
+                // Backtracking
                 board[row][col] = '.';
             }
         }
     }
 
+    
 
-
-    static List < String > construct(char[][] board) {
-        List < String > res = new LinkedList < String > ();
-        for (int i = 0; i < board.length; i++) {
-            String s = new String(board[i]);
-            res.add(s);
+    public static boolean isSafe(char[][] board, int row, int col) {
+        // Check vertical up
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
         }
-        return res;
+        // Check diagonal left up
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        // Check diagonal right up
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
     }
+
 
 }
