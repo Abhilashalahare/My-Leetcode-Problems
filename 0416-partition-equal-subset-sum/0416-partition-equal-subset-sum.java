58d3614368385bd23e86613f5e483d10f1e7b7f1
Abhilashalahare <166977285+Abhilashalahare@users.noreply.github.com>
2025-05-24 01:20:47 +0530

    Time: 71 ms (24.71%), Space: 48.8 MB (31.97%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,22 +1,48 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-          int sum = 0, n = nums.length;
-        
-        for (int i = 0; i < n; i++)
-            sum += nums[i];
-        
-        if (sum % 2 != 0) 
-            return false;
-
-        boolean[] dp = new boolean[10001];
-        dp[0] = true;
-        for (int num : nums) {
-            for (int j = 10000; j >= num; j--) {
-                dp[j] = dp[j] || dp[j - num];
-            }
-            if (dp[sum / 2]) 
-                return true;
+        int sum = 0;
+        int n = nums.length;
+        for (int i : nums) {
+            sum += i;
         }
-        return dp[sum / 2];
+
+        if (sum % 2 != 0) return false;
+
+        return fun(nums, sum / 2, n);
+    }
+
+    private Boolean fun(int arr[], int sum, int n) {
+     
+       
+        boolean[][] t = new boolean[n+1][sum+1];
+        
+        
+        for(int i=0; i<n+1; i++){
+            for(int j=0; j<sum+1; j++){
+                if(i==0) t[i][j] = false; //0th row
+                if(j==0) t[i][j] = true; //0th column
+            }
+        }
+        
+        
+       for(int i=1; i<n+1; i++){
+           for(int j=1; j<sum+1; j++){
+               if(j >= arr[i-1]){
+                t[i][j] = t[i-1][j-arr[i-1]] || t[i-1][j];
+               }else{
+                   t[i][j] =  t[i-1][j];
+               }
+           }
+       }
+        
+        
+        
+        return t[n][sum];
+        
+        
+        
+        
+        
+        
     }
 }
