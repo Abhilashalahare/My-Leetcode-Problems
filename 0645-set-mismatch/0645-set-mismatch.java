0d5b0e6e97c77ac97f071c305e274af2f0c00eca
Abhilashalahare <166977285+Abhilashalahare@users.noreply.github.com>
2025-07-14 17:55:00 +0530

    Time: 16 ms (12.13%), Space: 45.1 MB (98.51%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int repeatNum = repeat(nums);
+        int naturalSum= naturalSum(nums);
+        int actualSum =0;
+
+        for(int i=0; i<nums.length; i++){
+          actualSum += nums[i];
+        } 
+
+        int missingNum = naturalSum - (actualSum - repeatNum);
+
+        int[] ans = new int[2];
+        ans[0] = repeatNum;
+        ans[1] = missingNum;
+
+        return ans;
+    }
+
+    public int repeat(int[] nums){
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+           map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+        }
+
+        for(int i : nums){
+            if(map.get(i) > 1){
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    public int naturalSum(int[] nums){
+        int sum =0;
+        int n = nums.length;
+
+        int naturalSum = (n*(n+1))/2;
+
+        return naturalSum;
+    }
+}
