6d0929c5d3ae32100c55483c61c9abf9acc7b50d
Abhilashalahare <166977285+Abhilashalahare@users.noreply.github.com>
2025-07-31 09:06:08 +0530

    Time: 0 ms (100%), Space: 42.4 MB (55.38%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,22 +1,28 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-    int left = 0; // Initialize the left boundary of the search space
-        int right = nums.length - 1; // Initialize the right boundary of the search space
+        int n = nums.length;
+        if(n==1) return 0;
+        if(nums[0] > nums[1]){
+            return 0;
+        }
+        if(nums[n-1] > nums[n-2]) return n-1;
+
+    int left = 1; // Initialize the left boundary of the search space
+        int right = n-2; // Initialize the right boundary of the search space
       
         // Continue the loop until the search space is reduced to one element
         while (left < right) {
             // Calculate the middle index of the current search space
             int mid = (left + right ) / 2;
           
-            // If the middle element is greater than its next element, then a peak must be to the left (including mid)
-            if (nums[mid] > nums[mid + 1]) {
-                // Narrow the search space to the left half
-                right = mid;
-            } else {
-                // Otherwise, the peak exists in the right half (excluding mid)
-                // Narrow the search space to the right half
-                left = mid + 1;
-            }
+           if(nums[mid-1]< nums[mid] && nums[mid]>nums[mid+1]) return mid;
+
+           if(nums[mid]> nums[mid+1]){
+            right = mid-1;
+           }else{
+            left = mid+1;
+           }
+            
         }
       
         // When left == right, we have found the peak element's index, return it
