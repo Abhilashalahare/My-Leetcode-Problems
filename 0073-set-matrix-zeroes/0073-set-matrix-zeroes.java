57764b7c9f2c12082252f6741bf7c316d63e9e9e
Abhilashalahare <166977285+Abhilashalahare@users.noreply.github.com>
2025-11-04 18:46:26 +0530

    Time: 1 ms (67.6%), Space: 47.5 MB (6.45%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -3,33 +3,61 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
 
-        int[][] temp = new int[m][n];
+        int[] row = new int[m];
+        int[] col = new int[n];
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                temp[i][j] = matrix[i][j];
-            }
-        }
-
-      
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == 0) {
-                    for (int k = 0; k < n; k++) {
-                        temp[i][k] = 0;  // zero out the entire row
-                    }
-                    for (int k = 0; k < m; k++) {
-                        temp[k][j] = 0;  // zero out the entire column
-                    }
+                  row[i] = 1;
+                  col[j] = 1;
                 }
             }
         }
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                matrix[i][j] = temp[i][j];
-            }
-        }
-        
+                if(row[i] == 1 || col[j] == 1){
+                    matrix[i][j] = 0;
+                }
+               }
+               }
+               
+
     }
 }
+
+// class Solution {
+//     public void setZeroes(int[][] matrix) {
+//         int m = matrix.length;
+//         int n = matrix[0].length;
+
+//         int[][] temp = new int[m][n];
+
+//         for (int i = 0; i < m; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 temp[i][j] = matrix[i][j];
+//             }
+//         }
+
+//         for (int i = 0; i < m; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 if (matrix[i][j] == 0) {
+//                     for (int k = 0; k < n; k++) {
+//                         temp[i][k] = 0;  // zero out the entire row
+//                     }
+//                     for (int k = 0; k < m; k++) {
+//                         temp[k][j] = 0;  // zero out the entire column
+//                     }
+//                 }
+//             }
+//         }
+
+//         for (int i = 0; i < m; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 matrix[i][j] = temp[i][j];
+//             }
+//         }
+
+//     }
+// }
