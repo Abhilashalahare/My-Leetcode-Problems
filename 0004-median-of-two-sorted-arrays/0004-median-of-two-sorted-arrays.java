93f3faa0c22e1844c3a65aaba6758d4aa87f53bf
Abhilashalahare <166977285+Abhilashalahare@users.noreply.github.com>
2025-12-15 15:22:46 +0530

    Time: 2 ms (46.81%), Space: 49 MB (19.13%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -0,0 +1,51 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n=nums1.length;
+        int m =nums2.length;
+           int i=0; int j=0; int k=0;
+        int arr[] = new int[m+n];
+        int res=0;
+
+        while(i<n && j<m){
+            if(nums1[i]<nums2[j]){
+                arr[k] = nums1[i];
+                k++;
+                i++;
+            
+            }else{
+                arr[k] = nums2[j];
+                k++;
+                j++;
+
+            }
+          
+        }
+
+        while(i<n){
+            arr[k] = nums1[i];
+            k++;
+            i++;
+        }
+
+        while(j<m){
+            arr[k] = nums2[j];
+            k++;
+            j++;
+        }
+       
+      int l=0, r=arr.length-1;
+       
+      
+           int mid = l+(r-l)/2;
+
+           if(arr.length%2 ==0){
+            //even 
+           return (arr[mid] + arr[mid+1]) / 2.0;
+           }else{
+            return arr[mid]; //java automatically treat it as double arr[mid] becoz of the return type
+            // Java performs implicit type conversion from int → double
+           }
+       
+        
+    }
+}
