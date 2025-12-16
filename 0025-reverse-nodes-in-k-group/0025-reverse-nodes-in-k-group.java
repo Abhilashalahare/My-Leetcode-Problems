6e2d96e8ead60a1ad0809f499e77f239db7f420b
Abhilashalahare <166977285+Abhilashalahare@users.noreply.github.com>
2025-12-16 18:27:07 +0530

    Time: 0 ms (100%), Space: 46.4 MB (18.09%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -14,25 +14,28 @@ class Solution {
         ListNode prev = null;
         while (temp != null) {
             ListNode kth = findKthNode(temp, k);
-            if (kth == null) {
+            if (kth == null) {//2 cases, i)if size is smaller than kth, ii)at last finding kth is not possible bcos no kth group is left
                 if (prev !=  null) {
-                    prev.next = temp;
+                 prev.next = temp;//ii)case //because temp reverse hi ni hua, qki reverse krne k liye k group bcha hi nhi
                 } //if prev node is there, then only connect it to next head which is temp
-                break;
+                break;//i)case //in case of ll of size 2, prev node is pointing to null, becoz no kth node was found as k=3, so we have no group of size k, break and at least return head;
             }
         
 
-        ListNode nextNode = kth.next;
-        kth.next = null;
+        ListNode nextNode = kth.next;//preverse next node
+        kth.next = null;//break node
         reverse(temp);
         
-        if(temp == head){
+        if(temp == head){//at first time 
             head = kth;
         }else{
+            // on reversing kth comes at first, so prev next connected to first node of next group
+         
             prev.next = kth;
         }
-
+        // ṭemp starting tha, on reversing it became last, so prev will point to next group's lastnode
          prev = temp;
+        //  temp will point to start node of another group
          temp = nextNode;
         }
 
