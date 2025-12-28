e74caa001b2c89bc1dde16aa94cd3b3b2bad002c
Abhilashalahare <166977285+Abhilashalahare@users.noreply.github.com>
2025-12-28 16:17:21 +0530

    Time: 0 ms (100%), Space: 46.9 MB (39.38%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
@@ -7,6 +7,20 @@ class Solution {
         int col = n - 1;
         int count = 0;
 
+//         Because rows and columns are sorted decreasingly:
+
+// Start from top-right corner.
+
+// If the current number is negative, then everything below it in that column is also negative.
+
+// So add all remaining elements in that column.
+
+// Move left.
+
+// If the current number is non-negative, move down.
+
+// This avoids scanning every element.
+
         while (row < m && col >= 0) {
             if (grid[row][col] < 0) {
                 // All elements below in this column are negative
